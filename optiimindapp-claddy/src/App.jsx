a735e3554d2c0427737139a75115561857386b37
sanket <telisanket2002@gmail.com>
2023-05-23 18:16:16 +0530

    testing

--- optiimindapp-claddy/src/App.jsx
+++ optiimindapp-claddy/src/App.jsx
@@ -3,6 +3,7 @@ import { lazy, Suspense } from "react";
 import { About, Contact, Feedbacks, Hero, Navbar, Works, StarsCanvas } from "./components";
 import TalktoDoc from "./components/TalktoDoc";
 import Home from "./components/Home";
+import TestRoute from "./components/TestRoute";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route exact path="/chat" element={<TalktoDoc />} />
         <Route exact path="/" element={<Home></Home>} />
+        <Route exact path ="/test" element={<TestRoute></TestRoute>}></Route>
 
       </Routes>
     </BrowserRouter>
