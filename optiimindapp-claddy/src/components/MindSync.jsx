fa4787f2c12349e2be022cac66edd453ab623e6f
sanket <telisanket2002@gmail.com>
2023-05-25 08:43:50 +0530

    addded OPtiiMINd AI in mindsync

--- optiimindapp-claddy/src/components/MindSync.jsx
+++ optiimindapp-claddy/src/components/MindSync.jsx
@@ -1,106 +1,139 @@
 import React, { useState } from 'react';
 import '../styles1/mindsync.css'
 const MindSync = () => {
-  const [response, setResponse] = useState({
-    emotionalState: '',
-    happinessLevel: '',
-    stressLevel: '',
-    energyLevel: '',
-    physicalSymptoms: false,
-    concentrationDifficulties: false,
-    positiveEvents: false,
-    challenges: false,
-    sleepQuality: '',
-    lifeChanges: false
-  });
+    const API_KEY = import.meta.env.VITE_API_KEY;
+    async function feedData(response) {
+         
+        
 
-  const handleInputChange = (e) => {
-    const { name, value, type, checked } = e.target;
-    const updatedValue = type === 'checkbox' ? checked : value;
+        let systemMessage = {
+            "role": "user",
+            "content": "act as a content suggestor where you should provide me content based the parameters which I am providing you to boost my mood"
+        };
 
-    setResponse((prevResponse) => ({
-      ...prevResponse,
-      [name]: updatedValue
-    }));
-  };
+        var prompt = {"role": "user", "content":`Following are my mood defining things: 1.emotional state :${response.emotionalState}based on this tell me a joke or a poem and also suggest some songs and fun videos on youtube`}
+        console.log(prompt);
+        const apiRequestBody = {
+            "model": "gpt-3.5-turbo",
+            "messages": [
+                systemMessage,
+                prompt
+            ]
+        }
+        const data = await fetch("https://api.openai.com/v1/chat/completions", {
+            method: "POST",
+            headers: {
+                "Authorization": "Bearer " + API_KEY,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(apiRequestBody) // Change 'data' to 'body'
+        }).then((data) => {
+            return data.json();
+        }).then((data) => {
+            console.log(data);
+        });
+        console.log("the data is " + data.choices[0].message.content)
+    }
+    const [response, setResponse] = useState({
+        emotionalState: '',
+        happinessLevel: '',
+        stressLevel: '',
+        energyLevel: '',
+        physicalSymptoms: false,
+        concentrationDifficulties: false,
+        positiveEvents: false,
+        challenges: false,
+        sleepQuality: '',
+        lifeChanges: false
+    });
 
-  return (
-    <div className="mood-assessment">
-      <h2>Mood Assessment</h2>
-      <label>
-        How would you describe your current emotional state?
-        <br />
-        <input
-          type="radio"
-          name="emotionalState"
-          value="Happy"
-          onChange={handleInputChange}
-        /> Happy
-        <input
-          type="radio"
-          name="emotionalState"
-          value="Sad"
-          onChange={handleInputChange}
-        /> Sad
-        <input
-          type="radio"
-          name="emotionalState"
-          value="Angry"
-          onChange={handleInputChange}
-        /> Angry
-        <input
-          type="radio"
-          name="emotionalState"
-          value="Anxious"
-          onChange={handleInputChange}
-        /> Anxious
-        <input
-          type="radio"
-          name="emotionalState"
-          value="Neutral"
-          onChange={handleInputChange}
-        /> Neutral
-      </label>
+    const handleInputChange = (e) => {
+        const { name, value, type, checked } = e.target;
+        const updatedValue = type === 'checkbox' ? checked : value;
 
-      <label>
-        On a scale of 1 to 10, how would you rate your happiness level right now?
-        <br />
-        <input
-          type="number"
-          name="happinessLevel"
-          min="1"
-          max="10"
-          onChange={handleInputChange}
-        />
-      </label>
+        setResponse((prevResponse) => ({
+            ...prevResponse,
+            [name]: updatedValue
+        }));
+    };
 
-      {/* More questions... */}
+    return (
+        <div className="mood-assessment">
+            <h2>Mood Assessment</h2>
+            <label>
+                How would you describe your current emotional state?
+                <br />
+                <input
+                    type="radio"
+                    name="emotionalState"
+                    value="Happy"
+                    onChange={handleInputChange}
+                /> Happy
+                <input
+                    type="radio"
+                    name="emotionalState"
+                    value="Sad"
+                    onChange={handleInputChange}
+                /> Sad
+                <input
+                    type="radio"
+                    name="emotionalState"
+                    value="Angry"
+                    onChange={handleInputChange}
+                /> Angry
+                <input
+                    type="radio"
+                    name="emotionalState"
+                    value="Anxious"
+                    onChange={handleInputChange}
+                /> Anxious
+                <input
+                    type="radio"
+                    name="emotionalState"
+                    value="Neutral"
+                    onChange={handleInputChange}
+                /> Neutral
+            </label>
 
-      <label>
-        Are you currently experiencing any physical symptoms such as headaches, fatigue, or muscle tension?
-        <br />
-        <input
-          type="checkbox"
-          name="physicalSymptoms"
-          onChange={handleInputChange}
-        />
-      </label>
+            <label>
+                On a scale of 1 to 10, how would you rate your happiness level right now?
+                <br />
+                <input
+                    type="number"
+                    name="happinessLevel"
+                    min="1"
+                    max="10"
+                    onChange={handleInputChange}
+                />
+            </label>
 
-      <label>
-        Are you finding it difficult to concentrate or stay focused on tasks?
-        <br />
-        <input
-          type="checkbox"
-          name="concentrationDifficulties"
-          onChange={handleInputChange}
-        />
-      </label>
+            {/* More questions... */}
 
-      {/* More questions... */}
+            <label>
+                Are you currently experiencing any physical symptoms such as headaches, fatigue, or muscle tension?
+                <br />
+                <input
+                    type="checkbox"
+                    name="physicalSymptoms"
+                    onChange={handleInputChange}
+                />
+            </label>
 
-      <button onClick={() => console.log(response)}>Submit</button>
-    </div>
-  );
+            <label>
+                Are you finding it difficult to concentrate or stay focused on tasks?
+                <br />
+                <input
+                    type="checkbox"
+                    name="concentrationDifficulties"
+                    onChange={handleInputChange}
+                />
+            </label>
+
+            {/* More questions... */}
+
+            <button onClick={() => feedData(response)}>Submit</button>
+        </div>
+    );
 };
 
 export default MindSync;
